156a847a297a32d2c20f9b74d16dde8a383654f5
Artur Kot <arturkot85@gmail.com>
2019-11-19 20:48:19 +0100

    Format main.js with Prettier

--- js/main.js
+++ js/main.js
@@ -12,21 +12,16 @@ import {
 const getRandom = (min, max) => Math.random() * (max - min) + min;
 const { innerWidth, innerHeight } = window;
 const scene = new Scene();
-const camera = new PerspectiveCamera(
-    75,
-    innerWidth / innerHeight,
-    0.1,
-    1000
-);
+const camera = new PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 const renderer = new WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(
-    innerWidth,
-    innerHeight
-);
+renderer.setSize(innerWidth, innerHeight);
 
-const color = new Color(getComputedStyle(document.documentElement)
-    .getPropertyValue('--color-distinct').trim());
+const color = new Color(
+    getComputedStyle(document.documentElement)
+        .getPropertyValue("--color-distinct")
+        .trim()
+);
 const totalNumber = Math.max(50, Math.floor(window.innerWidth / 10));
 const dummies = new Array(totalNumber).fill().map(() => new Object3D());
 const speeds = new Array(totalNumber).fill(0).map(() => getRandom(0.01, 0.02));
@@ -44,26 +39,30 @@ function animate() {
     renderer.render(scene, camera);
 
     speeds.forEach((speed, index) => {
-    	dummies[index].position.set(-totalNumber * 0.04 + index / (totalNumber * 0.04), positionsY[index], 0);
-    	dummies[index].rotation.x += speed;
-    	dummies[index].rotation.y += speed;
-    	dummies[index].updateMatrix();
-    	cubes.setMatrixAt(index, dummies[index].matrix);
+        dummies[index].position.set(
+            -totalNumber * 0.04 + index / (totalNumber * 0.04),
+            positionsY[index],
+            0
+        );
+        dummies[index].rotation.x += speed;
+        dummies[index].rotation.y += speed;
+        dummies[index].updateMatrix();
+        cubes.setMatrixAt(index, dummies[index].matrix);
     });
 
     cubes.instanceMatrix.needsUpdate = true;
 }
-const onWindowResize = ()=> {
+const onWindowResize = () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
-}
+};
 
 animate();
 
-renderer.domElement.classList.add('animated-bg');
+renderer.domElement.classList.add("animated-bg");
 document.body.appendChild(renderer.domElement);
 renderer.domElement.addEventListener("touchmove", event => {
     event.preventDefault();
 });
-window.addEventListener('resize', onWindowResize, false);
+window.addEventListener("resize", onWindowResize, false);
