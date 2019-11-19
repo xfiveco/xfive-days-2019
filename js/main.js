7093c4ec99a2043000649c0b572d4a813113a9b1
Artur Kot <arturkot85@gmail.com>
2019-11-19 21:41:55 +0100

    Animation: update colors depending on media query

--- js/main.js
+++ js/main.js
@@ -9,20 +9,28 @@ import {
     Color
 } from "https://unpkg.com/three@0.110.0/build/three.module.js";
 
+let color = new Color(
+    getComputedStyle(document.documentElement)
+        .getPropertyValue("--color-distinct")
+        .trim()
+);
+let bgColor = new Color(
+    getComputedStyle(document.documentElement)
+        .getPropertyValue("--color-background")
+        .trim()
+);
+
 let leftBoundary = 0;
 let rightBoundary = 0;
+const media = matchMedia("(prefers-color-scheme: dark)");
 const { innerWidth, innerHeight } = window;
 const scene = new Scene();
 const camera = new PerspectiveCamera(45, innerWidth / innerHeight, 0.1, 1000);
 const renderer = new WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(innerWidth, innerHeight);
+renderer.setClearColor(bgColor, 1);
 
-const color = new Color(
-    getComputedStyle(document.documentElement)
-        .getPropertyValue("--color-distinct")
-        .trim()
-);
 const totalNumber = Math.max(10, Math.floor(window.innerWidth / 50));
 const dummies = new Array(totalNumber).fill().map(() => new Object3D());
 const speeds = new Array(totalNumber).fill(0).map(() => getRandom(0.001, 0.01));
@@ -75,6 +83,22 @@ function setScreenBoundaries() {
     rightBoundary = Math.tan(right) * adjacent * camera.aspect;
 }
 
+function updateColors() {
+    color = new Color(
+        getComputedStyle(document.documentElement)
+            .getPropertyValue("--color-distinct")
+            .trim()
+    );
+    bgColor = new Color(
+        getComputedStyle(document.documentElement)
+            .getPropertyValue("--color-background")
+            .trim()
+    );
+
+    material.color = color;
+    renderer.setClearColor(bgColor, 1);
+}
+
 setScreenBoundaries();
 renderer.domElement.classList.add("animated-bg");
 document.body.appendChild(renderer.domElement);
@@ -82,6 +106,7 @@ renderer.domElement.addEventListener("touchmove", event => {
     event.preventDefault();
 });
 window.addEventListener("resize", onWindowResize, false);
+media.addListener(updateColors);
 animate();
 setTimeout(() => {
     renderer.domElement.classList.add("is-ready");
